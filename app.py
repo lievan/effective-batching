6f379581a5aef5780e98006658393bcfa7374ae0
lievan <0evanli0@gmail.com>
2023-11-30 13:42:49 -0500

    basic app.py and batch_generate

--- app.py
+++ app.py
@@ -1,28 +1,84 @@
-from flask import Flask, request
 import json
-from inference import batch_generate
+import time
 
+from flask import Flask, request
+#from inference import batch_generate
+from threading import Thread, Lock, Event
 
 app = Flask(__name__)
 
+def batch_generate(next_batch):
+    # stub for testing
+    time.sleep(1)
+    print("running batch_generate")
+    results = []
+    for inference in next_batch:
+        results.append([inference.prompt, inference.job_id])
+    return results
+
+class Inference:
+    def __init__(self, job_id, prompt, num_tokens):
+        self.completion = None
+        self.job_id = job_id
+        self.prompt = prompt
+        self.num_tokens = num_tokens
+        self.event_obj = Event()
+
+    def finished(self, completion):
+        self.completion = completion
+        self.event_obj.set()
+
+    def wait_for_completion(self):
+        print("waiting for completion for job id {}".format(self.job_id))
+        self.event_obj.wait(1000)
+        return self.completion
+
 class InferenceManager:
-    pass
-    def enqueue(self);
-        pass
+    def __init__(self):
+        self.queue_mutex = Lock()
+        self.queue = []
+        self.running_inference = Lock()
+        self.simple_id = 0
+        self.inferences = {}
 
+    def enqueue(self, prompt, num_tokens):
+        job_id = None
+        new_inference = None
+        with self.queue_mutex:
+            job_id = self.simple_id
+            new_inference = Inference(job_id, prompt, num_tokens)
+            self.queue.append(new_inference)
+            self.inferences[job_id] = new_inference
+            self.simple_id += 1
+        return new_inference
 
+    def trigger_batch(self):
+        while True:
+            next_batch = []
+            results = []
+            with self.queue_mutex:
+                next_batch = self.queue
+                self.queue = []
+            if next_batch:
+                results = batch_generate(next_batch)
+                for result in results:
+                    completion, job_id = result
+                    self.inferences[job_id].finished(completion)
 
-def inference_manager(prompt: str):
-    # do some sort of qneueue
+manager = InferenceManager()
+run_inferences = Thread(target=manager.trigger_batch)
+run_inferences.start()
 
 @app.route('/inference', methods=['POST'])
 def hello():
     data = json.loads(request.get_data())
     prompt = data['prompt']
+    num_tokens = int(data['num_tokens'])
     assert isinstance(prompt, str)
-    result = inference_manager(prompt)
-    return {'completion': result}
-    
+    inference = manager.enqueue(prompt, num_tokens)
+    print("== INFERENCE ENQ ===")
+    completion = inference.wait_for_completion()
+    return {'completion': completion}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105, debug=True)
+    app.run(host='0.0.0.0', port=105, debug=True, threaded=True)
--- inference.py
+++ inference.py
@@ -11,56 +11,27 @@ enc = tiktoken.get_encoding("gpt2")
 enc = tiktoken.get_encoding("gpt2")
 encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
 decode = lambda l: enc.decode(l)
+init_from = 'gpt2-small' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
+seed = 1337
+device = 'cuda' if torch.cuda.is_available() else 'cpu' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
+compile = False # use PyTorch 2.0 to compile the model to be faster
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
+torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
+ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
+model = GPT.from_pretrained(init_from, dict(dropout=0.0))
 
-def setup():
-    # -----------------------------------------------------------------------------
-    init_from = 'gpt2-medium' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
-    out_dir = 'out' # ignored if init_from is not 'resume'
-    start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-    num_samples = 5 # number of samples to draw
-    max_new_tokens = 500 # number of tokens generated in each sample
-    temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
-    top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
-    seed = 1337
-    device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
-    dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
-    compile = False # use PyTorch 2.0 to compile the model to be faster
-    # exec(open('configurator.py').read()) # overrides from command line or config file
-    # -----------------------------------------------------------------------------
+model.eval()
+model.to(device)
+if compile:
+    model = torch.compile(model) # requires PyTorch 2.0 (optional)
 
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
-    torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
-    device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
-    ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
-    ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
-    # model
-    if init_from == 'resume':
-        # init from a model saved in a specific directory
-        ckpt_path = os.path.join(out_dir, 'ckpt.pt')
-        checkpoint = torch.load(ckpt_path, map_location=device)
-        gptconf = GPTConfig(**checkpoint['model_args'])
-        model = GPT(gptconf)
-        state_dict = checkpoint['model']
-        unwanted_prefix = '_orig_mod.'
-        for k,v in list(state_dict.items()):
-            if k.startswith(unwanted_prefix):
-                state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
-        model.load_state_dict(state_dict)
-    elif init_from.startswith('gpt2'):
-        # init from a given GPT-2 model
-        model = GPT.from_pretrained(init_from, dict(dropout=0.0))
 
-    model.eval()
-    model.to(device)
-    if compile:
-        model = torch.compile(model) # requires PyTorch 2.0 (optional)
-
-    return model
-
-def batch_generate(model, prompts, desired_tokens):
+def batch_generate(prompts, desired_tokens):
     assert len(prompts) == len(desired_tokens)
     prompts_and_desired_tokens = list(enumerate(desired_tokens))
     prompts_and_desired_tokens.sort(key=lambda x:x[1], reverse=True) #sort by num tokens
