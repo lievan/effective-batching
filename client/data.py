c78d3af2dc4d9434bea7c76abc0438eb2e49df19
lievan <0evanli0@gmail.com>
2023-12-07 15:46:54 -0500

    add normal

--- client/data.py
+++ client/data.py
@@ -21,7 +21,18 @@ class PromptData():
             self.prompt_idx += 1
       return next_sample
 
-  def generate_samples(self, num_samples):
+  def generate_samples_range(self, num_samples, range):
+      for i in range(num_samples):
+          target = random.choice(range)
+          prompt_len = max(1, abs(math.ceil(target)))
+          num_tokens = max(1, abs(math.ceil(target)))
+          prompt = ""
+          for i in range(prompt_len):
+            prompt += "{} ".format(i)
+          prompt = prompt.strip()
+          self.prompts.append((prompt, num_tokens))
+
+  def generate_samples_normal(self, num_samples):
       possible_num_tokens = random.normal(loc=100, size=(num_samples), scale=100)
       possible_prompt_len = random.normal(loc=100, size=(num_samples), scale=100)
 
