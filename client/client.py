194f772a97bdb70647f91b4b6683129d09377004
lievan <0evanli0@gmail.com>
2023-12-07 15:29:04 -0500

    verbose logs

--- client/client.py
+++ client/client.py
@@ -1,32 +1,50 @@
 import time
+import os
 import requests
+import random
+
 from threading import Thread
 from data import PromptData
+from dotenv import load_dotenv
+
+load_dotenv()
+
+KEY = os.getenv('KEY')
+IP = os.getenv('IP')
 
 NUM_SAMPLES=10
 
 prompt_data = PromptData(num_samples=NUM_SAMPLES)
 
-def inference_request(prompt, num_tokens):
+def inference_request(prompt, num_tokens, rid):
+    print("LOGS: Request {} with ~{} prompt len, {} tokens".format(rid, len(prompt.split(' ')), num_tokens))
+    auth_headers = {'Authorization':KEY}
     inference_req = {'prompt':prompt, 'num_tokens':num_tokens}
-    r = requests.post('http://127.0.0.1:8000/inference', json=inference_req)
+    r = requests.post('http://{}:8500/inference'.format(IP), json=inference_req, headers=auth_headers)
+    print("="*50)
+    print("LOGS: Got completion for request {}".format(rid))
     print(r.json()['completion'])
+    print("="*50)
+    print("")
 
 threads = []
 for i in range(NUM_SAMPLES):
     req, num_tokens = prompt_data.get_next_sample()
-    run_inference = Thread(target=inference_request, kwargs={"prompt": "request {}".format(i), "num_tokens": num_tokens})
-    threads.append(run_inference)
-
-for thread in threads:
-    thread.start()
-    time.sleep(0.1)
+    run_inference = Thread(target=inference_request, kwargs={"rid": i, "prompt": "request {}".format(i), "num_tokens": num_tokens})
+    run_inference.start()
+    if i % 5:
+        wait = random.random()
+        time.sleep(wait)
 
 for thread in threads:
     thread.join()
 
-r = requests.get('http://127.0.0.1:8000/stats')
-print("\n\n=== END STATS===\n\n")
-print(r.json())
+r = requests.get('http://{}:8500/stats'.format(IP))
+print("\n\n\n======== END STATS ========\n\n\n")
+stats = r.json()
+print(stats)
+with open('stats.txt', 'w') as f:
+    f.write(str(stats))
+
 
 
